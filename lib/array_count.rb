7311eab58e9556a61d286feada2d8df15f0b757a
James Sapp <jameseisapp@gmail.com>
2020-11-16 20:22:56 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,16 @@
+# Return the total number of strings in the provided array using the count enumerable
+
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+    array.count do |data|
+      data.class == String 
+  end 
 end
 
+
+# Return the total number of EMPTY strings in the provided array using the count enumerable
+
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |empty|
+    empty == ""
+  end 
 end
