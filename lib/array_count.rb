a666d60d3b1d57817d2b5ee61c111c422dfc6f10
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-24 21:53:54 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,25 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  total = 0
+  index = 0
+  while index < array.length do
+    if array[index].instance_of?(String)
+      total += 1
+    end
+    index += 1
+  end
+  total
 end
 
+count_strings([1,2,3,4])
+
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  total = 0
+  index = 0
+  while index < array.length do
+    if array[index] == ""
+      total += 1
+    end
+    index += 1
+  end
+  total
 end
